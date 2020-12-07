55be7ef9cfb20e44b43acfc65efa37748fd489cd
Christian Garrovillo <me@christiang.dev>
2020-12-06 17:56:14 -0700

    totally didnt expose a django key

--- anothernotetaker/settings.py
+++ anothernotetaker/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(1#qwz3a37o!kz60q50@j+_ok3%mhv9=k=3+%!#nc_8*aid7a-'
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
